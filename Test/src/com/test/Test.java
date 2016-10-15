9b1e7ed89e8070aec0f70790b620f1ee1c62a067
rvaja <rvaja@localhost>
2016-10-14 19:01:50 -0700

    Initial commit.

---
+++ Test/src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str = "Ri,tesh'vaja";
+
+		System.out.println(str.replaceAll("[+>.\']", ""));
+
+	}
+
+}
